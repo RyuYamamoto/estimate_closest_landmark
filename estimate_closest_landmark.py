380c19c36580c887b868591c9356cf42bacffa92
RyuYamamoto <ryu.yamamoto04@g.softbank.co.jp>
2019-08-08 17:29:16 +0900

    その場で見えなかった場合の処理を追加

--- estimate_closest_landmark.py
+++ estimate_closest_landmark.py
@@ -74,7 +74,7 @@ class EstimateLandmark:
     #    (※)その中でも動く必要がある場合はなるべく移動量の少なくて済むランドマークを推定したい
     # TODO
     # ロボット、ランドマークそれぞれのベクトル向きを計算し、それらのなす角度の誤差を計算し一番誤差が少ないものを選択する？
-    def get_closest_landmark(self, robot_pose, landmark):
+    def get_closest_landmark_non_move(self, robot_pose, landmark):
         side_range = [-math.radians(70), math.radians(70)]
         min_distance = 100
         min_landmark_id = -1
@@ -89,24 +89,72 @@ class EstimateLandmark:
             # ロボットから見たランドマークの位置・姿勢
             t_robot2landmark = t_map2robot.inverse() * t_map2landmark
             robot2landmark = almath.position6DFromTransform(t_robot2landmark)
-            print(id, robot2landmark, robot2landmark.norm())
-
-            local_vector = almath.position6DFromPose2D(almath.Pose2D(1.0, 0, 0))
-            # ロボットとランドマークのなす角度を計算する(x,y平面)
-            a = local_vector.x*robot2landmark.x
-            b = math.sqrt(local_vector.x*local_vector.x)*math.sqrt(robot2landmark.x*robot2landmark.x+robot2landmark.y*robot2landmark.y)
-            local_angle_xy = math.acos(a/b)
-
-            a1 = local_vector.x*robot2landmark.x
-            b1 = math.sqrt(local_vector.x*local_vector.x)*math.sqrt(robot2landmark.x*robot2landmark.x+robot2landmark.z*robot2landmark.z)
-            local_angle_xz = math.acos(a1/b1)
+            #print(id, robot2landmark, robot2landmark.norm())
+            
+            local_angle_xy, local_angle_xz = self._get_angle_vector(robot2landmark)
 
             if (side_range[0] < local_angle_xy < side_range[1]) and (side_range[0] < local_angle_xz < side_range[1]):
                 if robot2landmark.norm() < min_distance:
                     min_distance = robot2landmark.norm()
                     min_landmark_id = id
-            print(math.degrees(local_angle_xy))
+            #print(math.degrees(local_angle_xy))
         return min_landmark_id
+
+    # 上の関数でどのランドマークも条件に当てはまらなかった場合に呼ばれる
+    # ランドマーク全てに対して距離となす角度(xy, xz二通り)を計算し、もっとも移動量の少なくて済む
+    # ランドマークを取得する
+    # 条件順位：下記条件を満たすランドマークを取得する TODO: この優先順位だとnullが帰ってくるパターンがある
+    # 1. xz面において任意の角度内に収まっている
+    # 2. xy面においてベクトル同士のなす各がすべてのランドマークにたいして最小
+    # 3. ランドマークとロボット間の距離が認識できる最大・最小距離内に収まっている
+    def get_closest_landmark(self, robot_pose, landmark):
+        distance_info = dict()
+        side_range = [-math.radians(70), math.radians(70)]
+        for id in landmark:
+            # calculate angle robot to landmark
+            # マップ原点座標から見たランドマークの位置・姿勢
+            map2landmark = almath.Position6D(landmark[id])
+            t_map2landmark = almath.transformFromPosition6D(map2landmark)
+            # map原点座標から見たロボットの位置・姿勢
+            map2robot = almath.position6DFromPose2D(robot_pose)
+            t_map2robot = almath.transformFromPosition6D(map2robot)
+            # ロボットから見たランドマークの位置・姿勢
+            t_robot2landmark = t_map2robot.inverse() * t_map2landmark
+            robot2landmark = almath.position6DFromTransform(t_robot2landmark)
+
+            local_angle_xy, local_angle_xz = self._get_angle_vector(robot2landmark)
+
+            # 1. xz面において任意の角度内に収まっている
+            # TODO この時点でnullが帰ってくる可能性がある
+            print(math.degrees(local_angle_xz))
+            #if (side_range[0] < local_angle_xz < side_range[1]):
+                # ベクトルのノルムを格納する
+            distance_info[id] = local_angle_xy#robot2landmark.norm()
+
+        # 2. xy面においてベクトル同士のなす角がすべてのランドマークに対して最小か
+        #    1.でフィルタしたランドマークリストを元に角度が小さい順にソートする
+        #min_k = min(d, key=d.get)
+        #min_landmark_id = min(distance_info, key=distance_info.get)
+        distance_info = sorted(distance_info.items(), key=lambda x:x[1])
+        print(distance_info)
+        return distance_info
+
+        # 3. 一番角度が小さいランドマークを取得する。この時3.の認識できる距離の範囲内にランドマークが存在しているかをチェックする
+
+    def _get_angle_vector(self, robot2landmark):
+        # ロボットのローカル座標を示す一時的な位置ベクトル
+        local_vector = almath.position6DFromPose2D(almath.Pose2D(1.0, 0, 0))
+        # ロボットとランドマークのなす角度を計算する(x,y平面)
+        a1 = local_vector.x*robot2landmark.x
+        b1 = math.sqrt(local_vector.x*local_vector.x)*math.sqrt(robot2landmark.x*robot2landmark.x+robot2landmark.y*robot2landmark.y)
+        local_angle_xy = math.acos(a1/b1)
+
+        # ロボットとランドマークのなす角度を計算する(x,z平面)
+        a2 = local_vector.x*robot2landmark.x
+        b2 = math.sqrt(local_vector.x*local_vector.x)*math.sqrt(robot2landmark.x*robot2landmark.x+robot2landmark.z*robot2landmark.z)
+        local_angle_xz = math.acos(a2/b2)
+
+        return (local_angle_xy, local_angle_xz)
 
 def load_landmark():
     txt = open("map.yaml", "r")
@@ -120,13 +168,18 @@ if __name__ == "__main__":
     marker_list = list()
     marker_list = load_landmark()
 
-    initial_robot_pose = almath.Pose2D(2,0,-almath.PI/2)
+    initial_robot_pose = almath.Pose2D(1,1,-almath.PI/2)
     while True:
         initial_robot_pose.theta = initial_robot_pose.theta + 0.05
-        closest_landmark_id = estimate.get_closest_landmark(initial_robot_pose, marker_list)
-
         visualize.config_screen()
+
+        closest_landmark_id = estimate.get_closest_landmark_non_move(initial_robot_pose, marker_list)
         if closest_landmark_id != -1:
+            visualize.draw_circle(marker_list[closest_landmark_id][0], marker_list[closest_landmark_id][1])
+        else:
+            temp_landmark = estimate.get_closest_landmark(initial_robot_pose, marker_list)
+            #closest_landmark_id = min(temp_landmark, key=temp_landmark.get)
+            closest_landmark_id = temp_landmark[0][0]
             visualize.draw_circle(marker_list[closest_landmark_id][0], marker_list[closest_landmark_id][1])
         visualize.draw_landmark(marker_list)
         visualize.move_robot(initial_robot_pose.toVector())
